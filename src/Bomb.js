a2c6573a2db63df54c233c9445cd15fb21976d95
Miranda Bonilla <bonillamiranda3@gmail.com>
2019-07-09 22:56:48 +0000

    Automatically backed up by Learn

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,16 @@
 // your Bomb code here!
+
+import React frin 'react';
+
+export default class Bomb extends React.component {
+  constructor(props)
+  super()
+  this.state = {
+    secondsLeft: prop.initialCount
+  }
+}
+
+
+  render() {
+    const response = this.state.secondsLeft === 0 ? 'Boom!'
+  }
